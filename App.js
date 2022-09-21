d2b3ed52f341116294aa57da9bc9464daf36f7b7
tdat3023 <tdat3023@gmail.com.com>
2022-09-21 14:34:26 +0700

    screen1

---
+++ App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import Screen1 from './Screen/Screen1';
+import Screen2 from './Screen/Screen2';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    < NavigationContainer >
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Screen1} />
+        <Stack.Screen name="Option" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer >
+  );
+}
